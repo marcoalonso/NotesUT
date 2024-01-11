b4f0de075573df4d6ccfbc938bf8e15d6d137816
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-01-11 12:36:55 -0600

    testUpdateNote added

--- NotasUTTests/ViewModelTests.swift
+++ NotasUTTests/ViewModelTests.swift
@@ -31,7 +31,55 @@ final class ViewModelTests: XCTestCase {
         XCTAssertEqual(vm.notes.first?.title, title)
     }
     
-    
+    func testCreateThreeNotes() {
+        //Given
+        let title1 = "Test Tilte1"
+        let text1 = "Text Test1"
+        
+        let title2 = "Test Tilte2"
+        let text2 = "Text Test2"
+        
+        let title3 = "Test Tilte3"
+        let text3 = "Text Test3"
+        
+        //Then
+        vm.createNoteWith(title: title1, text: text1)
+        vm.createNoteWith(title: title2, text: text2)
+        vm.createNoteWith(title: title3, text: text3)
+        
+        //When
+        XCTAssertEqual(vm.notes.count, 3)
+        XCTAssertEqual(vm.notes.first?.text, text1)
+        XCTAssertEqual(vm.notes.first?.title, title1)
+        XCTAssertEqual(vm.notes[1].text, text2)
+        XCTAssertEqual(vm.notes[1].title, title2)
+        XCTAssertEqual(vm.notes[2].text, text3)
+        XCTAssertEqual(vm.notes[2].title, title3)
+    }
 
+    func testUpdateNote() {
+        //Given
+        let title = "Test Tilte"
+        let text = "Text Test"
+        
+        vm.createNoteWith(title: title, text: text)
+        
+        let newTitle = "New Test Tilte"
+        let newText = "New Text Test"
+        
+        //When
+        if let id = vm.notes.first?.id {
+            vm.updateNoteWith(id: id, newTitle: newTitle, newText: newText)
+            
+            //Then
+            XCTAssertEqual(vm.notes.first?.text, newText)
+            XCTAssertEqual(vm.notes.first?.title, newTitle)
+            
+        } else {
+            XCTFail("No note was created.")
+        }
+        
+        
+    }
 
 }
